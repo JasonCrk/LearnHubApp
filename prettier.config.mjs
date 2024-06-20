f4501529e5abb182fa3334c9713f142304096ed5
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2024-06-20 15:25:53 -0500

    feat: Project base

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+const config = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+}
+
+export default config
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import { CssBaseline } from '@mui/material'
+
+import { RouterProvider } from 'react-router-dom'
+
+import { routes } from './routes'
+
+import '@fontsource/roboto/100.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import '@fontsource/roboto/900.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <CssBaseline />
+    <RouterProvider router={routes} />
+  </StrictMode>,
 )
